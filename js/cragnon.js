4fda0c05b56302e05c448eac32c0cea3af2ae906
Nicholas Bannister-Andrews <nbannist@home>
2012-10-19 17:14:25 -0400

    Added .style()/.styles()/.selectorFromElement()
    style() & styles() - Getting and Setting of css properties.
    .selectorFromElement() - given an element, returns a CSS selector rule
    from it's attributes (ID, class).

--- js/cragnon.js
+++ js/cragnon.js
@@ -1,9 +1,17 @@
-/*
+	/*
 	Cragnon Javascript Library
 	set up is similar to how jQuery sets it's self up
+
+	[SIP] = still in progress
+
+	TODO: replace document/element.querySelector[All]() 
+		with a function that can be overridden for browsers 
+		that don't have support for qSA.
 */
 
 (function (window, undefined) {
+	"use strict";
+
 	var doc = window.document, // shortcut/alias 
 		cragLib = function (selector, context, options) { // cragnon function
 			return new cragLib.prototype.init(selector, context, options); // make a new object here so the dev doesn't need to use "new"
@@ -12,20 +20,98 @@
 
 		cragLib.prototype = {
 			init: function (selector, context, options) {
-				var self = this;
-
 				// TODO: conditions that may change selector, context, and even options before sending it to makeCragnon()
-
 				return cragLib.prototype.makeCragnon(selector, context, options);
 			},
-			makeCragnon: function (selector, context, options) {
-				var self = this;
 
-				// TODO: setup a cragnon object (self/this) and return it to callee.
+			// [SIP]
+			makeCragnon: function (selector, context, options) {
+				var self = this,
+					elements = [];
+
+				if (!context) {
+					//console.log('context is undefined');
+					context = doc;
+				} //
+
+				// string; need to make a cragnon object
+				if (typeof context === 'string') {
+					// selector should be prepended with the context
+					//console.log('context is a string (selector) ');
+					self.selector = context + ' ' + selector;
+					self.context = doc;
+				} // cragnon object; get selector;
+				else if (__.isCragnon(context)) {
+					//console.log('context is a cragnon obj');
+					self.selector = context.selector + ' ' + selector;
+					self.context = doc;
+				} // node (element || document)
+				else if (context.nodeType) {
+					//console.log('context is an element || window.document');
+					self.selector = selector;
+					self.context = context; // element should be the context
+				}
+				else { // default is the document;
+					//console.log('context is set to doc');
+					self.context = doc;
+					self.selector = selector;
+				}
+
+				if (self.context && self.selector) {
+					// elements = self.context.querySelectorAll(self.selector);
+					elements = self.selectAll(self.selector, self.context);
+					self.elements = __.nodeListToArray(elements);
+				} 
+				else {
+					self.elements = null;
+				}
+				
 
 				return self;
 			},
-			context: '[object cragnon]',
+
+			// [SIP]
+			// find descendents within the current context (this.context || document);
+			// find is normally a "findAll()"
+			find: function (selector, options) {
+				var self = __.clone(this),
+					elements = [];
+
+				self.context = this.context || doc;
+				self.selector = self.selector + ' ' + selector;
+				// elements = self.context.querySelectorAll(self.selector);
+				elements = self.selectAll(self.selector, self.context);
+				self.elements = __.nodeListToArray(elements);
+
+				return self;
+			},
+
+			// more descriptive version.
+			findAll: function (selector, options) {
+				return find(selector, options);
+			},
+
+			// only returns up to one (if there are any to begin with)
+			findFirst: function (selector, options) {},
+
+			// This is used in place of document.querySelectAll()/element.querySelectAll()
+			// This is so it can be overriden and replaced for browsers/contexts that don't have
+			// those functions available.
+			selectAll: function (selector, context) {
+				if (!context) {
+					context = doc;
+				}
+				return context.querySelectorAll(selector);
+			},
+			/* 
+			other select functions?
+			select: function () {},
+			selectFirst: function () {},
+			selectLast: function () {},
+			*/
+
+			context: doc, // default is window.document; should change to be context of previous  
+			constructor: '[object cragnon]',
 			elements: null,
 			length: 0,
 
@@ -33,12 +119,41 @@
 			nodeListToArray: function (nodeList) {
 				var i = 0, 
 					newArray = [];
+
 				for (i = 0; i < nodeList.length; i++) {
 					newArray.push(nodeList[i]);
 				}
 				newArray = __.removeDuplicateNodes(newArray);
 
 				return newArray;
+			},
+			removeDuplicateNodes: function (elementArray) {
+				/* TEST */
+				var i = 0,
+					k = 0,
+					e, // elements; same as nodes really, just needed a different name
+					n; // nodes
+
+				for (i = 0; i < elementArray.length; i++) {
+					e = elementArray[i]; // element
+
+					for ( ; k < elementArray.length; k++) {
+						
+						if (i !== k) { // make sure we're not looking at the same index
+							n = elementArray[k]; // node
+
+							if (n === e) {
+								console.log('Samesies!');
+								console.log('i[' + i + ']/k[' + k + ']');
+								console.dir(e);
+								console.dir(n);
+								elementArray.splice(i, 1);
+							}
+						}
+					}
+					k = 0;
+				}
+				return elementArray;
 			},
 
 			//[OK] clone object
@@ -109,13 +224,12 @@
 			//[OK] // is nodes a NodeList object?
 			isNodeList: function (nodes) {
 				// credit: http://stackoverflow.com/questions/332422/how-do-i-get-the-name-of-an-objects-type-in-javascript
-				// 
 				var result = Object.prototype.toString.call(nodes);
 
 				if (typeof nodes === 'object' && 
 					/^\[object (HTMLCollection|NodeList|Object)\]$/.test(result) && // HTMLCollection/Object must be used in non-webkit browsers.
 					nodes.hasOwnProperty('length') &&
-					(nodes.length == 0 || (typeof nodes[0] === "object" && nodes[0].nodeType > 0))) {
+					(nodes.length === 0 || (typeof nodes[0] === "object" && nodes[0].nodeType > 0))) {
 					return true;
 				}
 				return false; //...otherwise...
@@ -129,8 +243,136 @@
 				}
 				return false;
 			},
-			// , ...
+
+			// given an element returns a CSS selector as specific as it can be 
+			// (or that you ask for) // not yet coded.
+			selectorFromElement: function (element, options) {
+				var selector = '', 
+					id = '', 
+					classes = '',
+					attributes = '',
+					classRegEx = /[ ]/gi;
+
+				if (element.nodeType && element !== window.document) {
+					selector = element.tagName.toLowerCase(); 
+
+					// add ID
+					if (element.id && element.id !== '') {
+						// id
+						selector = selector + '#' + element.id; // (like the Highlander, there can only be one.)
+						// TODO: catch if ID is illegal?
+					}
+					// add class(es)
+					if (element.className && element.className !== '') {
+						// className
+						classes = element.className.replace(classRegEx, '.');
+						selector = selector + '.' + classes;
+					}
+					return selector;
+				} // if element === document, then return empty string.
+				else {
+					return '';
+				}
+			},
+
+			// Changing Styles
+			// styles(properties)
+			// @properties can be an object with properties and values we should change: 
+			// {"border-radius": "5px", ...},
+			// or an array of strings of css properties whose values we should return.
+			// ["border-radius", "width", "height"] --> 
+			// {"border-radius": :5px", "width": "100px", "height": "200px"}
+			styles: function (properties) {
+				// this just loops through the properties and calls style(property, value)
+				var self = this,
+					el = {},
+					arrayIndex = 0,
+					values = [],
+					property = "",
+					value = "";
+
+				if (__.isObject(properties)) {
+					for (property in properties) {
+						if (properties.hasOwnProperty(property)) {
+							// call style(property, value)
+							if (properties[property]) {
+								value = properties[property];
+								self.style(property, value); // sets
+							}
+							else {
+								value = self.style(property); // returns {"property": "value"}
+								values.push(value);
+							}
+						}
+					}
+				}
+				else if (__.isArray(properties)) {
+					for (arrayIndex = 0; arrayIndex < properties.length; arrayIndex++) {
+						if (typeof properties[arrayIndex] === 'string') {
+							property = properties[arrayIndex];
+							value = self.style(property);
+							values.push(value);
+						}
+						else if (__.isObject(properties[arrayIndex])) {
+							for (property in properties[arrayIndex]) {
+								// filter out possible inherited properties
+								if (properties[arrayIndex].hasOwnProperty(property)) {
+									// check to make sure explicitly not undefined and not null 
+									// zero is falsy so we don't want to make the condition generic
+									if (typeof properties[arrayIndex][property] !== 'undefined' && properties[arrayIndex][property] !== null) {
+										self.style(property, properties[arrayIndex][property]);
+									}
+									else {
+										values.push(JSON.parse('{"' + property + '": "' + self.style(property) + '"}'));
+									}
+								}
+							}
+						}
+					}
+				}
+
+				if (values.length > 0) {
+					return values;
+				}
+				// return self if only setting 
+				return self;
+			},
+
+			// for changing/returning a single style
+			// property is a string, value is of the appropriate type 
+			style: function (property, value) {
+				var self = this,
+					el = {},
+					elementIndex = 0,
+					values = [];
+
+				if (self.elements.length > 0) {
+					// console.log('loop through elements');
+					for (elementIndex = 0; elementIndex < self.elements.length; elementIndex++) {
+						el = self.elements[elementIndex]; // alias
+						if (el.nodeType) { // is an element?
+							if (typeof value === 'undefined' || value === null) { // get/return value
+								values.push(JSON.parse('{"' + property + '": "' + document.defaultView.getComputedStyle(el, null).getPropertyValue(property) + '"}'));
+							} // set value instead
+							else {
+								el.style[property] = value;
+							}
+						}
+					}
+				}
+
+				if (values.length > 1) {
+					return values;
+				} else if (values.length > 0) {
+					return values[0]; // more than 0 but less than 2: only return first item
+				}
+
+				return self;
+			}
+			// , ... more functions!
+
 		};
+
 		// methods for uninitialized cragnon object (e.g., "_." or "cragnon." )
 		// methods that would be useful even if you don't have an immediate 
 		// need to operate on elements
@@ -141,6 +383,7 @@
 		cragLib.isNumber = cragLib.prototype.isNumber;
 		cragLib.isObject = cragLib.prototype.isObject;
 		cragLib.makeCragnon = cragLib.prototype.makeCragnon;
+		cragLib.selectorFromElement = cragLib.prototype.selectorFromElement;
 		
 
 	// shortcut so init()'s prototype is the same as cragnon's prototype
@@ -149,7 +392,6 @@
 	__ = cragLib.prototype; // alias; double-underscore
 
 
-
 	window.cragnon = window._ = cragLib;
-} (window = window || this));
+} (window || this));
 
--- js/tests.js
+++ js/tests.js
@@ -1,6 +1,6 @@
 /*
 	Tests for cragnon.js
-	// TODO: upgrade to a unit testing library.
+	// TODO: upgrade to a unit testing library. Build suite, etc.
 	// http://omar.gy/how-i-ended-up-enjoying-javascript/
 */
 
@@ -8,26 +8,117 @@
 
 	function runTests() {
 		test0010(); // v0.0.1
-		//test0020(); // v0.0.2
-		//test0030(); // v0.0.3
+		test0020(); // v0.0.2
+		testStyles();
+		// testGoogleCode();
 		// see the pattern? :D
 	}
 	window.runTests = runTests;
 
 	function test0010() { // test for v0.1.0
-		var crObj = _(), // initialized version (different function from uninited version)
+		var crObj = _(), // initialized version (different function from un-inited version)
 			crLib = _; // uninitialized version (different functions from inited version)
 
-		console.dir(crLib); 
-		console.dir(crObj); 
-
-
-		
-
-
+		//console.dir(crLib);
+		//console.dir(crObj);
 	}
 
 	// more tests as more functionality is added
 	// ....
+	function test0020() {
+		var crObj = _('ul li.odd'),
+			crObj2 = _('ul').find('li.odd');
+
+		testselectorFromElement();
+		//console.dir(crObj);
+		//console.dir(crObj2);
+	}
+
+	function testselectorFromElement() {
+		var element = document.querySelectorAll('#search_area')[0],
+			selector = _.selectorFromElement(element);
+
+		//console.log('testselectorFromElement()');
+		//console.dir(element);
+		//console.log(selector);
+
+	}
+
+	function testStyles() {
+		var crObj = _('#search_area'),
+			styles1 = '',
+			styles2 = '',
+			styles3 = '';
+
+
+		// STYLES
+
+		// styles([properties])
+		styles1 = crObj.styles(['width', 'height']); // return width/height
+			console.dir(styles1);
+			
+		crObj.styles([{'width': '100px'}, {'height': '100px'}]); // set width of element to 100px
+
+		// styles({properties with null values})
+		styles2 = crObj.styles({'width': undefined, 'height': undefined}); // return width/height
+			console.dir(styles2);
+
+		styles3 = crObj.styles({'width': '150px', 'height': null, 'border-radius': 0});
+		// return array of height; sets value for width.
+			console.dir(styles3);
+
+		// styles({properties with proper values})
+		crObj.styles({'width': '200px', 'height': '200px'}); // set width of element to 200px
+		
+
+		// STYLE
+
+
+		crObj.style('width');
+		crObj.style('width', '200px').style('height', '200px');
+		console.dir(crObj.styles(['width','height']));
+	}
+	/* 
+	//Commented out. To put in its own file at a later time.
+	function testGoogleCode() {
+		var isInTree = function (value, tree) {
+
+			if (typeof value === 'undefined' || value === null) { //return false if there is no value to evaluate
+				return false;
+			}
+			
+			// because tree.value can have 0, we have to use typeof instead of tree.value, which would normally evaluate to false if tree.value didn't exist.
+			if (typeof tree !== 'object' || typeof tree.value === 'undefined') { // needs to have at least .value
+				return false;
+			}
+
+			if (tree.value === value) { //is this the value we're looking for?
+				return true;
+			}
+			else if (tree.value < value && tree.more) { // if this tree.value is too small and there is a greater option
+				return isInTree(value, tree.more);
+			}
+			else if (tree.value > value && tree.less) { // if this tree.value is too large and there is a lesser option
+				return isInTree(value, tree.less);
+			}
+
+			return false;
+		};
+
+		var myTree = {"value": 2, "less": {"value": 1, "less": {"value": 0}}, "more": {"value": 10, "less": {}}};		
+
+		console.log(isInTree(9, myTree)); // false
+		console.log(isInTree(2, myTree)); // true
+		console.log(isInTree(0, myTree)); // true
+		console.log(isInTree(10, myTree)); // true
+		console.log(isInTree(1, myTree)); // true
+		console.log(isInTree(0)); // false
+		console.log(isInTree(0, {})); // false
+
+	} 
+	// end of long comment section
+	*/ 
+
 
 } (window, undefined));
+
